e69a32bcbee96a5be3e0e42c95e251226263b60e
courtoi223 <courtoi223@unicaen.fr>
2024-10-15 23:27:26 +0200

    VendWithbug

--- script.js
+++ script.js
@@ -27,7 +27,24 @@ let obstacleY = boardHeight - obstacleHeight;
 //repeat for other 
 let obstacle1Img;
 
+//physics
+let velocityx = -8;
+let velocityY = 0;
+let gravity = .4;
+
+let gameOver = false;
+let score = 0;
+
+let  player = {
+    x : pX,
+    y : pY,
+    width : pWidth,
+    height : pHeight
+}
+
+
 window.onload = function(){
+
     board.height = boardHeight;
     board.width = boardWidth;
 
@@ -38,27 +55,62 @@ window.onload = function(){
     context.fillStyle="red";
     context.fillRect(pX,pY,pWidth,pHeight);
 
-    // pImg = new Image();
-    // pImg.src ="./img/....."
-    // pImg.onload = function(){
-    // context.drawImage(pImg,pX,pY,pWidth,pHeight);
-    // }
+    pImg = new Image();
+    pImg.src ="./img/idlle.png";
+    pImg.onload = function(){
+    context.drawImage(pImg,pX,pY,pWidth,pHeight);
+    }
 
-    // obstacle1Img = new Image();
-    // obstacle1Img.src ="./img/....."
+    obstacle1Img = new Image();
+    obstacle1Img.src ="./img/obstacle1.png";
 
 
     requestAnimationFrame(update);
     setInterval(placeObstacle,1000);
+    document.addEventListener("keydown",moveDirectionPlayer);
+
 }
 
 function update(){
     requestAnimationFrame(update);
+    if(gameOver){
+        return;
+    }
+
+    context.clearRect(0,0,boardWidth,boardHeight);
+    //player
+    velocityY += gravity;
+    player.y = Math.min(player.y + velocityY,pY);
     context.drawImage(pImg,pX,pY,pWidth,pHeight);
+    //obstacle
+    for(let i = 0; i < obstacleArray.length;i++){
+        let obstacle = obstacleArray[i];
+        obstacle.x += velocityx;
+        context.drawImage(obstacle.img,obstacle.x,obstacle.width,obstacle.height);
+
+        if(detectCollision(player,obstacle)){
+            gameOver = true;
+
+        }
+    }
+}
+
+function moveDirectionPlayer(e){
+    if(gameOver){
+        return;
+    }
+
+    if((e.code == "Space" || e.code == "ArrowUp") && player.y == pY){
+        velocityY = -10;
+
+    }
 
 }
 
 function placeObstacle(){
+    if(gameOver){
+        return;
+    }
     let obstacle = {
         img : null,
         x : obstacleX,
@@ -77,5 +129,14 @@ function placeObstacle(){
 
     // }
 
+    if(obstacleArray.length > 5){
+        obstacleArray.shift();
+    }
+}
 
+function detectCollision(a,b){
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
 }
